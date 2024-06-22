954562381a56f1fab32f9967d5ccda2b6040f5dc
code-qtzl <azepeda90@outlook.com>
2024-06-22 16:00:25 -0500

    map: adding test file

--- lesson_map_test.py
+++ lesson_map_test.py
@@ -1,27 +1,58 @@
-def change_bullet_style(document):
-    # Easiest way to do this challenge.
-    # change = document.replace('-', '*')
-    # return change
-    # ======================================
+from lesson_map import *
 
-    # Parameter:
-    # document (str): The input document as a string.
+run_cases = [
+    (
+        "* Alai\n- Dink Meeker\n",
+        "* Alai\n* Dink Meeker\n",
+    ),
+    (
+        "* Ender Wiggin\n- Petra Arkanian\n* Bean\n",
+        "* Ender Wiggin\n* Petra Arkanian\n* Bean\n",
+    ),
+]
 
-    # splitting doc into list of lines
-    lines = document.split('\n')
-    # passes convert_line function to each line of the input string
-    changed_lines = map(convert_line, lines)
-    # joins the modified lines back together
-    result = '\n'.join(changed_lines)
-    return result
-    
+submit_cases = run_cases + [
+    (
+        "- Bonzo Madrid\n- Stilson\n- The Formics\n- Peter Wiggin\n- Valentine Wiggin\n- Colonel Graff\n",
+        "* Bonzo Madrid\n* Stilson\n* The Formics\n* Peter Wiggin\n* Valentine Wiggin\n* Colonel Graff\n",
+    ),
+]
 
 
-# Don't edit below this line
+def test(input_document, expected_output):
+    print("---------------------------------")
+    print("Input document:")
+    print(input_document)
+    print("Expected output:")
+    print(expected_output)
+    result = change_bullet_style(input_document)
+    print("Actual output:")
+    print(result)
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
 
-def convert_line(line):
-    old_bullet = "-"
-    new_bullet = "*"
-    if len(line) > 0 and line[0] == old_bullet:
-        return new_bullet + line[1:]
-    return line
+
+def lesson_map():
+    passed = 0
+    failed = 0
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+lesson_map()
