51b316ed447ab5f0a473ed8de0077b75468adecc
code-qtzl <azepeda90@outlook.com>
2024-07-23 23:59:57 -0500

    ch4: update to tree file

--- Ch-4/tree.py
+++ Ch-4/tree.py
@@ -1,16 +1,12 @@
 def list_files(current_filetree, current_path=""):
     file_paths = []
 
-    for node in current_filetree:
-        if current_path:
-            new_path = f"/{current_path}/{node}"
-        else:
-            new_path = node
+    for node, subtree in current_filetree.items():
+        new_path = f"{current_path}/{node}" if current_path else f"/{node}"
             
-        if current_filetree[node] is None:
+        if subtree is None:
             file_paths.append(new_path)
         else:
-            file_paths.extend(list_files(current_filetree[node], new_path))
-    
+            file_paths.extend(list_files(subtree, new_path))
     
     return file_paths
