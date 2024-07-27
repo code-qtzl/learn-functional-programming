739554a65a167365afb01e84ffa097aa37ca5e18
code-qtzl <azepeda90@outlook.com>
2024-07-27 16:01:07 -0500

    lfp: ch5 challenge - filter

---
+++ Ch-5/filterMain.py
@@ -0,0 +1,25 @@
+def get_filter_cmd(filter_one, filter_two):
+    def filter_cmd(content, option='--one'):
+
+        if option == '--one':
+            return filter_one(content)
+        if option == '--two':
+            return filter_two(content)
+        if option == '--three':
+            return filter_two(filter_one(content))
+        
+        if option not in ['--one', '--two', '--three']:
+            raise ValueError('invalid option')
+
+    return filter_cmd
+
+def replace_bad(text):
+    return text.replace("bad", "good")
+
+
+def replace_ellipsis(text):
+    return text.replace("..", "...")
+
+
+def fix_ellipsis(text):
+    return text.replace("....", "...")
